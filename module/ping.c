cc4549bee25b2856b0892810723d2324f2a97577
kagakusixya <kagakusixya@pumpkin.Pumpkin>
2018-06-21 07:10:16 -1000

    icmpinfoの初期化

--- module/ping.c
+++ module/ping.c
@@ -1,9 +1,10 @@
 #include<sys/socket.h>
 #include<stdio.h>
-#include <netinet/ip_icmp.h>
+#include<netinet/ip_icmp.h>
+#include<string.h>
 /*ping*/
 int ping(char sendip,char myip ){
-  struct icmphdr icmphdrinfo;
+  struct icmphdr icmphdrinfo; //icmp用のヘッダー
   int sock;//sock用
   sock = socket(AF_INET, SOCK_RAW,0);
   //SOCK_RAW ->ip or icmp AF_INET ipv4
@@ -11,5 +12,9 @@ if (sock < 0) {
   perror("socket err");
   return 1;
 }
+/*icmphdrinfoにデータを代入*/
+
+//icmphdrinfo初期化
+memset(&icmphdrinfo,0,sizeof(icmphdrinfo));
   return 0;
 }
