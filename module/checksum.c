dc7c1c71d6129906e5dcddc6df78449123bbd5b7
kagakusixya <kagakusixya@pumpkin.Pumpkin>
2018-06-21 08:23:18 -1000

    テンプレートの代入

--- module/checksum.c
+++ module/checksum.c
@@ -1,2 +1,14 @@
-void checksum(){
+unsigned short checksum(unsigned short *buf, int bufsz){//checksumのテンプレート
+  unsigned long sum = 0;
+  while (bufsz > 1) {
+    sum += *buf;
+    buf++;
+    bufsz -= 2;
+  }
+  if (bufsz == 1) {
+    sum += *(unsigned char *)buf;
+  }
+  sum = (sum & 0xffff) + (sum >> 16);
+  sum = (sum & 0xffff) + (sum >> 16);
+  return ~sum;
 }
