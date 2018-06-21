9939b3d43aeaddbed66987a36af39f7997aa0fb8
kagakusixya <kagakusixya@pumpkin.Pumpkin>
2018-06-21 08:59:12 -1000

    pingテスト

--- main.c
+++ main.c
@@ -1,4 +1,12 @@
+#include<stdio.h>
 #include "module/ping.h"
 int main(int argc,char *argv[]){
+  int pin =ping(*argv[1]);
+  if(pin==1){
+    printf("socket err\n");
+  }else if(pin ==2){
+    printf("sendto err\n");
+  }
+  
   return 0;
 }
