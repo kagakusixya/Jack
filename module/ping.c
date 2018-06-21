b2a6d5e02ba7ab38ed92c63da2f1363bb247ee53
kagakusixya <kagakusixya@pumpkin.Pumpkin>
2018-06-21 06:56:26 -1000

    socketのコメント変更

--- module/ping.c
+++ module/ping.c
@@ -5,6 +5,7 @@ int ping(char sendip,char myip ){
   //socketの作成
   int sock;
   sock = socket(AF_INET, SOCK_RAW,0);
+  //SOCK_RAW ->ip or icmp AF_INET ipv4
 if (sock < 0) {
   perror("socket err");
   return 1;
