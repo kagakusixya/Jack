0c1c16f99e06cd02a8bbdc9afa82055708fb0945
kagakusixya <kagakusixya@pumpkin.Pumpkin>
2018-06-21 07:27:51 -1000

    コメントの整理

--- module/ping.c
+++ module/ping.c
@@ -7,7 +7,7 @@ int ping(char sendip,char myip ){
   struct icmphdr icmphdrinfo; //icmp用のヘッダー
   int sock;//sock用
   sock = socket(AF_INET, SOCK_RAW,0);
-  //SOCK_RAW ->ip or icmp AF_INET ipv4
+  //SOCK_RAW ->ip or icmp ,AF_INET ->ipv4
 if (sock < 0) {
   perror("socket err");
   return 1;
