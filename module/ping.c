4f334281f32fc0ad2f7cb24f3eca9fdd1c605d26
kagakusixya <kagakusixya@pumpkin.Pumpkin>
2018-06-21 06:53:12 -1000

    socketの作成

--- module/ping.c
+++ module/ping.c
@@ -1,4 +1,13 @@
+#include<sys/socket.h>
+#include<stdio.h>
 /*ping*/
 int ping(char sendip,char myip ){
+  //socketの作成
+  int sock;
+  sock = socket(AF_INET, SOCK_RAW,0);
+if (sock < 0) {
+  perror("socket err");
+  return 1;
+}
   return 0;
 }
